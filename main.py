64a698a707fd12078774808a5cdcdfd703ab8cfa
braden <leachbr787@northwested.org>
2024-12-17 14:15:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,40 +2,57 @@
 #December 16 2024
 #Password Validation
 
-password = input('What is your password: ')
+#password = input('What is your password: ')
 
 #----part one----#
 
- #Check length
+#Check length
 def check_length(password):
     if len(password) < 8:
-        return 'Password must be at least 8 characters long.' 
+        print( 'Password must be at least 8 characters long.')
     return None 
     
 
- # Check First Five
+#Check First Five
 def check_first_five(password):
   if len(password) >= 5 and not password[0:5].isalpha():
-      return'The first five characters must be letters only!'
+      print('The first five characters must be letters only!')
   return None
     
 
-     #Check Last Three
+#Check Last Three
 def check_last_three(password):
     if len(password) >= 3 and not password[-3:].isdigit():
-       return 'The last three characters must be numbers only!'
+       print('The last three characters must be numbers only!') 
     return None
     
 
 
 
-#----part two----# 
+# #----part two----# 
 
-#Check special letter 
+# #Check for letter 
+def check_for_letter(password):
+   for char in password:
+      if char.isalpha():
+         return None
+   print('Password must contain at least one letter!') 
+        
+
+# #Check special letter 
 def check_no_special_letters(password):
    special_caracter = '~`!@#$%^&*()_+-={}[]:";\'<>?,./'
-   
+
    for char in password: 
       if char in special_caracter:
-         return 'Sorry password can not contain any special characters!'
+         print('Sorry password can not contain any special characters!') 
    return None
+
+
+# #Check for numbers 
+def check_for_number(password):
+   for char in password:
+      if char.isdigit():
+         return None
+   print('Password must contain at least one number!') 
+
